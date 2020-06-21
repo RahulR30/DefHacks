812dbb0f2e03e5b6ba2dddb6d9c5d9cf2356387c
Rahul <sdrahul.rao@gmail.com>
2020-06-20 17:26:24 -0700

    wad

--- DefHacksHackathon/ViewController.swift
+++ DefHacksHackathon/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var backgroundGradientView:UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [#colorLiteral(red: 0.2305614398, green: 0.6767108169, blue: 1, alpha: 1).cgColor, UIColor(red:255/255, green: 0/255,blue: 0/255, alpha:1).cgColor]
+        
+        gradientLayer.shouldRasterize = true
+        
+        backgroundGradientView.layer.addSublayer(gradientLayer)
+        
+    }
+    override var shouldAutorotate: Bool{
+        return false;
     }
 
 
